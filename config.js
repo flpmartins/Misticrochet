734cfde062578f1d7e87ba6431849519e95bf0f0
flpmartins <flpmartins@users.noreply.github.com>
2026-02-20 19:12:41 +0000

    deploy: 254feb1f3a811384fe2d91e0d2f974861e911781

---
+++ config.js
@@ -0,0 +1,6 @@
+const EMAILJS_CONFIG = {
+    PUBLIC_KEY: 'FJeglvoAiLjxJakPh',
+    SERVICE_ID: 'service_7yt1oia',
+    TEMPLATE_ID: 'template_sy42mk9',
+    TO_EMAIL: 'misticrochet47@gmail.com'
+};
